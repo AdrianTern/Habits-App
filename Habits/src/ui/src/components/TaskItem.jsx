c3996e57d20d83bcdb69679b4d8ea7232f0ad557
Adrian Tern <adriantern97@gmail.com>
2025-05-30 17:19:46 +0800

    Fixed console log errors

--- Habits/src/ui/src/components/TaskItem.jsx
+++ Habits/src/ui/src/components/TaskItem.jsx
@@ -1,23 +1,25 @@
-import { 
-    ListItem, 
-    ListItemButton, 
-    ListItemIcon, 
-    ListItemText, 
-    IconButton, 
-    Checkbox, 
+import {
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    IconButton,
+    Checkbox,
     Typography,
-    Box, 
+    Box,
 } from '@mui/material';
 import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
 import RadioButtonUncheckedRoundedIcon from '@mui/icons-material/RadioButtonUncheckedRounded';
 import RadioButtonCheckedRoundedIcon from '@mui/icons-material/RadioButtonCheckedRounded';
 import ErrorRoundedIcon from '@mui/icons-material/ErrorRounded';
-import { styled } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
 import { useState } from 'react';
 import dayjs from 'dayjs';
+import { motion } from 'framer-motion';
 
-function TaskItem({ task, onToggle, onEdit}) {
-    const[isCompleted, setIsCompleted] = useState(task.isCompleted);
+function TaskItem({ task, onToggle, onEdit }) {
+    const theme = useTheme();
+    const [isCompleted, setIsCompleted] = useState(task.isCompleted);
     const isOverdued = !isCompleted && dayjs(task.dueDate).isBefore(dayjs(), 'day');
 
     const handleToggle = () => {
@@ -32,13 +34,6 @@ function TaskItem({ task, onToggle, onEdit}) {
         }
     });
 
-    const PrettyListItem = styled(ListItem)({
-        transition: 'all 0.5s ease',
-        '&:hover': {
-            transform: 'scale(1.02)',
-        },
-    });
-
     const PrettyListItemButton = styled(ListItemButton)({
         transition: 'all 0.5s ease',
         '&:hover': {
@@ -46,76 +41,96 @@ function TaskItem({ task, onToggle, onEdit}) {
         },
     });
 
+    const TaskInfoText = styled(Typography)({
+        fontSize: '0.7rem',
+        [theme.breakpoints.up('sm')]: {
+            fontSize: '0.8rem'
+        },
+        color: theme.palette.custom.darkgrey,
+    })
+
+    const TaskInfo = () => {
+        return (
+            <Box>
+                <TaskInfoText>
+                    {task.description}
+                </TaskInfoText>
+                <TaskInfoText>
+                    {task.dueDate}
+                </TaskInfoText>
+            </Box>
+        )
+    }
+
     return (
-        <PrettyListItem 
+        <motion.li
+            layout
             key={task.id}
             className="task-item"
-            secondaryAction={(
-            <IconButton 
-                edge="end" 
-                aria-label="edit"
-                size='medium'
-                onClick={onEdit} 
-                sx={{ 
-                    color: 'black',
-                    visibility: isCompleted ? 'hidden' : 'visible',
-                }}>
-                <EditNoteRoundedIcon fontSize='medium'/>
-            </IconButton>
-            )}
         >
-            <PrettyListItemButton onClick={handleToggle} dense>
-                <ListItemIcon>
-                    <BlackCheckbox
-                        className="task-iscomplete"
-                        edge="start"
-                        checked={isCompleted}
-                        onClick={handleToggle}
-                        icon={<RadioButtonUncheckedRoundedIcon/>}
-                        checkedIcon={<RadioButtonCheckedRoundedIcon/>}
-                        color='black'
-                        disableRipple
+            <Box display='flex'>
+                <PrettyListItemButton onClick={handleToggle} dense>
+                    <ListItemIcon>
+                        <BlackCheckbox
+                            className="task-iscomplete"
+                            edge="start"
+                            checked={isCompleted}
+                            onClick={handleToggle}
+                            icon={<RadioButtonUncheckedRoundedIcon />}
+                            checkedIcon={<RadioButtonCheckedRoundedIcon />}
+                            color='black'
+                            disableRipple
+                            sx={{
+                                '& .MuiSvgIcon-root': {
+                                    fontSize: { xs: '1rem', sm: '1.5rem' },
+                                }
+                            }} />
+                    </ListItemIcon>
+                    <Box>
+                        <ListItemText
+                            className="task-title"
+                            sx={{
+                                textDecoration: isCompleted ? 'line-through' : 'none',
+                                color: isCompleted ? 'gray' : 'black'
+                            }}
+                            primary={
+                                <Box display="flex">
+                                    <Typography
+                                        variant='h6'
+                                        noWrap='false'
+                                        sx={{
+                                            fontSize: { xs: '0.9rem', sm: '1rem' },
+                                            mr: 1
+                                        }}
+                                    >
+                                        {task.title}
+                                    </Typography >
+                                    {isOverdued && <ErrorRoundedIcon fontSize='small' sx={{ color: 'custom.darkred', mt: 0.2 }} />}
+                                </Box>
+                            }
+                        />
+                        <TaskInfo/>
+                    </Box>
+                </PrettyListItemButton>
+                <IconButton
+                        edge="end"
+                        aria-label="edit"
+                        size='medium'
+                        onClick={onEdit}
                         sx={{
-                            '& .MuiSvgIcon-root':{
-                                fontSize:{xs: '1rem', sm: '1.5rem'},
-                            }                           
-                        }}/>
-                </ListItemIcon>
-                <ListItemText 
-                    className="task-title" 
-                    sx={{
-                        textDecoration: isCompleted ? 'line-through' : 'none',
-                        color: isCompleted ? 'gray' : 'black'
-                    }}
-                    primary={
-                        <Box display="flex">
-                            <Typography 
-                                variant='h6' 
-                                noWrap='false' 
-                                sx={{
-                                    fontSize: {xs: '0.8rem', sm: '1rem'},
-                                    mr: 1
-                                    }}
-                            >
-                                {task.title}
-                            </Typography >
-                            {isOverdued && <ErrorRoundedIcon fontSize='small' sx={{color: 'custom.darkred', mt:0.2}}/>}
-                        </Box>
-                        } 
-                    secondary={
-                        <>
-                        <Typography variant='body2' sx={{fontSize: {xs: '0.8rem', sm: '1rem'}}}>
-                            {task.description}
-                        </Typography>
-                        <Typography variant='caption' color="text.secondary">
-                            {task.dueDate}
-                        </Typography>
-                        </>
-                    }
-                    />
-            </PrettyListItemButton>
-        </PrettyListItem>
-    );  
-} 
+                            color: 'black',
+                            visibility: isCompleted ? 'hidden' : 'visible',
+                            marginRight: '0.5rem',
+                            transition: 'all 0.3s ease',
+                                '&:hover': {
+                                    transform: 'scale(1.10)',
+                                },      
+                        }}>
+                        <EditNoteRoundedIcon fontSize='medium' />
+                </IconButton>
+            </Box>
+        </motion.li>
+    );
+}
 
 export default TaskItem;
