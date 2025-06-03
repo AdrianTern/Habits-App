b888e71f0257a589762bf8e60aa768978d05396a
Adrian Tern <adriantern97@gmail.com>
2025-06-03 23:26:11 +0800

    Corrected task sorting criteria

--- Habits/src/ui/src/reducers/taskReducer.js
+++ Habits/src/ui/src/reducers/taskReducer.js
@@ -1,14 +1,28 @@
 // useReducer for managing tasks in a todo list application
 // This reducer function manages the state of tasks in a todo list application.
 // It handles actions such as setting tasks, adding a task, updating a task, toggling a task's completion status, and deleting a task.
+
+// Sort task by the following priority:
+// 1. isCompleted: Incomplete task always stay at the top, completed task always stay at the bottom
+// 2. isRoutineTask: Routine task should always stay at the top
+// 3. dueDate: Task without due date comes first, then sort ascendingly
 const sortTasks = (tasks) => {
     if(tasks){
         return [...tasks].sort((a,b) => {
             if(a.isCompleted !== b.isCompleted) return a.isCompleted ? 1 : -1;
-            return new Date(a.dueDate) - new Date(b.dueDate);
+            if(a.routineDetailsResponse.isRoutineTask !== b.routineDetailsResponse.isRoutineTask) return a.routineDetailsResponse.isRoutineTask ? -1 : 1;
+            return compareDates(a,b);
         });
     }
   }
+
+const compareDates = (a, b) => {
+    if(!a.dueDate && !b.dueDate) return 0;
+    if(!a.dueDate) return -1;
+    if(!b.dueDate) return 1;
+
+    return new Date(a.dueDate) - new Date(b.dueDate);
+}
 
 export const taskReducer = (state, action) => {
     switch(action.type) {
